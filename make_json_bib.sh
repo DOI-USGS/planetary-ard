fde58fcdec19416d445976a78eb479dedb9480e5
Laura, Jason R <jlaura@usgs.gov>
2023-02-28 14:45:07 -0700

    Adds themis docs

--- make_json_bib.sh
+++ make_json_bib.sh
@@ -1,3 +1,3 @@
 # Use citation-js to convert the bibtext bibliography into a JSON-CSL bibliography.
 # citation-js is a npm package
-/Users/jlaura-pr/.npm-global/lib/node_modules/citation-js/bin/cmd.js -i ardbib.bib > ~/github/arddocs/content/bibliography.json
+node_modules/citation-js/bin/cmd.js -i resources/ardbib.bib > content/bibliography.json
